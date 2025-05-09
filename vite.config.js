459d92b27c8baaa3d61b5a24897742ec8184fd9e
Yeachen29 <yeachenabir29@gmail.com>
2025-05-09 14:17:18 +0600

    Alphabet Pazzle Game

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "alphabet-pazzle",
+  base: "/alphabet-pazzle",
   server : {
     host : '0.0.0.0',
     port : "3000",
